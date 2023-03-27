0123b2ad984af940501ccbdcd44355428cbf7083
M Takken <martintakken@gmail.com>
2023-03-27 15:10:35 +0200

    account en category edit beter

--- tak-simple-bookkeeper/resources/views/livewire/booking-account-edit.blade.php
+++ tak-simple-bookkeeper/resources/views/livewire/booking-account-edit.blade.php
@@ -1,33 +1,23 @@
 <div>
 
     {{-- laten we leeg --}}
-    <x-slot name="header"></x-slot>
-
-
-
-
-
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight">
+            Settings voor {{ $account->name }}
+        </h2>
+    </x-slot>
 
 
     <div class="py-6">
         <div class="">
 
-            {{--  a div the aligns on the right side --}}
-
-
-            {{-- 'slug' => 'debiteuren',
-                'named_id' => 'debiteuren',
-                'name' => 'Debiteuren',
-                'include_children' => 1,
-                'intern' => 0,
-                'plus_min_int' => 1,
-                'start_balance' => 0, --}}
 
             <form wire:submit.prevent="save">
 
                 <div class="flex justify-center ">
+
                     <div wire:loading.class.remove="success-fader" wire:loading.class.remove="error-fader"
-                        class="max-w-lg overflow-hidden rounded shadow-lg
+                        class="overflow-hidden rounded shadow-lg
             
                         @if (session()->has('message')) @if (session('message') === 'success')
                             success-fader @endif
@@ -39,109 +29,117 @@
 
 
 
-                        <div class="flex justify-end mb-3">
+                        <div class="grid grid-cols-2 gap-4">
 
 
-
-
-                            @if ($account->plus_min_int === 1)
-                                <div class="text-white btn bg-takgreen-500 btn-big"><i class="fa fas fa-plus "
-                                        aria-hidden="true"></i>
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="account_name">Account naam</label><br />
+                                    <input type="text" wire:model.debounce.4s="account.name" placeholder="type.."
+                                        required>
                                 </div>
-                            @else
-                                <div class="text-white btn bg-takred-500 btn-big"><i class="fa fas fa-minus "
-                                        aria-hidden="true"></i>
-                                </div>
-                            @endif
-                        </div>
-
-                        <div class="flex flex-col gap-4 px-6 py-4 md:flex-row md:items-center md:justify-between">
-                            <h2 class="text-xl font-semibold leading-tight">
-                                Settings voor {{ $account->name }}
-                            </h2>
-                        </div>
-
-
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="account.name" placeholder="type.."
-                                    required>
-                            </div>
-                            <p class="text-sm text-gray-700">
-                                De naam van het account wordt gebruikt in de weergave van alle pagina's.
-                            </p>
-                        </div>
-
-
-
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="account.named_id" placeholder="type.."
-                                    required>
-                            </div>
-                            <p class="text-sm text-gray-700">
-                                Deze naam wordt intern gebruikt als account naam. Als je bank account bijvoorbeeld
-                                'NL94INGB0007001049' is dan
-                                kun je dat hier invullen. De boekingen worden dan ook opgeslagen met deze naam.
-                            </p>
-                        </div>
-
-
-
-
-
-
-
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="account.slug" placeholder="type.."
-                                    required>
-                            </div>
-                            <p class="text-sm text-gray-700">
-                                De slug wordt gebruikt bij het aanroepen van de pagina, zie de adres balk in je browser.
-                                Die moet je dus alleen aanpassen als je weet wat je doet.
-                            </p>
-                        </div>
-
-
-
-
-
-
-                        <div class="px-6 pt-4 pb-2">
-                            <div>
-
-                                <input type="text" wire:model.debounce.4s="account.start_balance" class="w-100"
-                                    class="numberinputfield" />
-
-
-                                <p class="mt-2 text-sm text-gray-700">
-                                    Het begin saldo van dit account.
+                                <p class="text-sm text-gray-700">
+                                    De naam van het account wordt gebruikt in de weergave van alle pagina's.
                                 </p>
                             </div>
 
-                            <div class="my-5">
 
 
-                                <div class="form-row">
-                                    <div class="pl-4 form-group col-md-1">
-                                        <input type="radio" checked wire:model="account.plus_min_int" value="1">
-                                        <label for="plus_min_int">{{ __('debet account') }}</label>
-                                    </div>
-                                    <div class="pl-4 form-group col-md-1">
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="account_named_id">Account keyname</label><br />
+                                    <input type="text" wire:model.debounce.4s="account.named_id" placeholder="type.."
+                                        required>
+                                </div>
+                                <p class="text-sm text-gray-700">
+                                    Deze naam wordt intern gebruikt als account naam. Als je bank account bijvoorbeeld
+                                    'NL94INGB0007001049' is dan
+                                    kun je dat hier invullen. De boekingen worden dan ook opgeslagen met deze naam.
+                                </p>
+                            </div>
 
-                                        <input type="radio" wire:model="account.plus_min_int" value="-1">
-                                        <label for="plus_min_int">{{ __('credit account') }}</label>
-                                    </div>
+
+                        </div>
+
+
+
+                        <div class="grid grid-cols-2 gap-4">
+
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="account_slug">Account slug</label><br />
+                                    <input type="text" wire:model.debounce.4s="account.slug" placeholder="type.."
+                                        required>
+                                </div>
+                                <p class="text-sm text-gray-700">
+                                    De slug wordt gebruikt bij het aanroepen van de pagina, zie de adres balk in je
+                                    browser.
+                                    Die moet je dus alleen aanpassen als je weet wat je doet.
+                                </p>
+                            </div>
+
+
+                            <div class="px-6 pt-4 pb-2">
+                                <div>
+                                    <label class="text-sm" for="account_slug">Start saldo</label><br />
+                                    <input type="text" wire:model.debounce.4s="account.start_balance" class="w-100"
+                                        class="numberinputfield" />
+
 
                                     <p class="mt-2 text-sm text-gray-700">
-                                        Debet accounts zijn accounts waarop je geld binnen krijgt. <br />
-                                        Credit accounts zijn accounts waarop je geld uitgeeft.
+                                        Het begin saldo van dit account.
                                     </p>
                                 </div>
 
-
                             </div>
+
+                        </div>
+
+
+                        <div class="grid grid-cols-2 gap-4">
+
+                            <div class="px-6 py-4 form-row">
+                                <label class="text-sm" for="account_plus_min_int">Debet of Credit</label><br />
+                                <div class="pl-4 form-group col-md-1">
+                                    <input type="radio" checked wire:model="account.plus_min_int" value="1">
+                                    <label for="plus_min_int">{{ __('debet account') }}</label>
+                                </div>
+                                <div class="pl-4 form-group col-md-1">
+
+                                    <input type="radio" wire:model="account.plus_min_int" value="-1">
+                                    <label for="plus_min_int">{{ __('credit account') }}</label>
+                                </div>
+
+                                <p class="mt-2 text-sm text-gray-700">
+                                    Debet accounts zijn accounts waarop je geld binnen krijgt. <br />
+                                    Credit accounts zijn accounts waarop je geld uitgeeft.
+                                </p>
+                            </div>
+
+
+
+
+
+                            <div>
+                                <div class="flex justify-end px-6 py-4 mb-3">
+
+                                    @if ($account->plus_min_int == 1)
+                                        <div class="text-white btn bg-takgreen-500 btn-big"><i class="fa fas fa-plus "
+                                                aria-hidden="true"></i>
+                                        </div>
+                                    @else
+                                        <div class="text-white btn bg-takred-500 btn-big"><i class="fa fas fa-minus "
+                                                aria-hidden="true"></i>
+                                        </div>
+                                    @endif
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div class="grid grid-cols-2 gap-4 px-6 py-4">
+
+
                             <div>
                                 <x-button type="submit" class="justify-center w-48 mt-5">
                                     {{ __('Save') }}
@@ -151,7 +149,7 @@
 
 
                             @if (isset($account))
-                                <div class="mt-10">
+                                <div class="mt-5">
                                     <button
                                         class="px-4 py-1 text-sm text-black bg-gray-100 border border-gray-600 rounded hover:bg-gray-300">
                                         <a href="{{ route('account.onaccount', $account->slug) }}">
@@ -162,20 +160,23 @@
                             @endif
 
                         </div>
+
+
                     </div>
                 </div>
-
-
-            </form>
-
-
-
-
-
         </div>
 
 
+        </form>
+
+
+
+
+
     </div>
+
+
+</div>
 
 
 
--- tak-simple-bookkeeper/resources/views/livewire/booking-category-edit.blade.php
+++ tak-simple-bookkeeper/resources/views/livewire/booking-category-edit.blade.php
@@ -1,12 +1,11 @@
 <div>
 
     {{-- laten we leeg --}}
-    <x-slot name="header"></x-slot>
-
-
-
-
-
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight">
+            Settings voor categorie: {{ $category->name }}
+        </h2>
+    </x-slot>
 
 
     <div class="py-6">
@@ -17,7 +16,7 @@
 
                 <div class="flex justify-center ">
                     <div
-                        class="max-w-lg overflow-hidden rounded shadow-lg
+                        class="overflow-hidden rounded shadow-lg
             
                         @if (session()->has('message')) @if (session('message') === 'success')
                             success-fader @endif
@@ -29,92 +28,82 @@
 
 
 
-                        <div class="flex justify-end mb-3">
 
+                        <div class="grid grid-cols-2 gap-4">
 
-
-
-                            @if ($category->plus_min_int === 1)
-                                <div class="text-white btn bg-takgreen-500 btn-big"><i class="fa fas fa-plus "
-                                        aria-hidden="true"></i>
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="category_name">Categorie naam</label><br />
+                                    <input type="text" wire:model.debounce.4s="category.name" placeholder="type.."
+                                        required>
                                 </div>
-                            @else
-                                <div class="text-white btn bg-takred-500 btn-big"><i class="fa fas fa-minus "
-                                        aria-hidden="true"></i>
+                                <p class="text-sm text-gray-700">
+                                    De naam van de categorie wordt gebruikt in de weergave van alle pagina's.
+                                </p>
+                            </div>
+
+
+
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="category_named_id">Categorie keyname</label><br />
+                                    <input type="text" wire:model.debounce.4s="category.named_id"
+                                        placeholder="type.." required>
                                 </div>
-                            @endif
-                        </div>
-
-                        <div class="flex flex-col gap-4 px-6 py-4 md:flex-row md:items-center md:justify-between">
-                            <h2 class="text-xl font-semibold leading-tight">
-                                Settings voor categorie: {{ $category->name }}
-                            </h2>
-                        </div>
-
-
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="category.name" placeholder="type.."
-                                    required>
+                                <p class="text-sm text-gray-700">
+                                    Deze naam wordt intern gebruikt als categorie naam. Deze moet uniek zijn.
+                                </p>
                             </div>
-                            <p class="text-sm text-gray-700">
-                                De naam van de categorie wordt gebruikt in de weergave van alle pagina's.
-                            </p>
+
+
                         </div>
 
 
 
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="category.named_id" placeholder="type.."
-                                    required>
+
+
+
+                        <div class="grid grid-cols-2 gap-4">
+
+
+
+                            <div class="px-6 py-4">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="category_slug">Categorie slug</label><br />
+                                    <input type="text" wire:model.debounce.4s="category.slug" placeholder="type.."
+                                        required>
+                                </div>
+                                <p class="text-sm text-gray-700">
+                                    De slug wordt gebruikt bij het aanroepen van de pagina, zie de adres balk in je
+                                    browser.
+                                    Die moet je dus alleen aanpassen als je weet wat je doet.
+                                </p>
                             </div>
-                            <p class="text-sm text-gray-700">
-                                Deze naam wordt intern gebruikt als categorie naam. Deze moet uniek zijn.
-                            </p>
-                        </div>
 
 
 
+                            <div class="px-6 py-4 ">
+                                <div class="mb-2">
+                                    <label class="text-sm" for="opmerkingen">Opmerkingen</label><br />
+                                    <textarea class="w-full text-sm" wire:model.debounce.4s="category.remarks" placeholder="type.."></textarea>
 
-
-
-
-                        <div class="px-6 py-4">
-                            <div class="mb-2">
-                                <input type="text" wire:model.debounce.4s="category.slug" placeholder="type.."
-                                    required>
+                                </div>
+                                <p class="text-sm text-gray-700">
+                                    Opmerkingen
+                                </p>
                             </div>
-                            <p class="text-sm text-gray-700">
-                                De slug wordt gebruikt bij het aanroepen van de pagina, zie de adres balk in je browser.
-                                Die moet je dus alleen aanpassen als je weet wat je doet.
-                            </p>
+
                         </div>
 
 
-
-                        <div class="px-6 py-4 ">
-                            <div class="mb-2">
-
-                                <textarea class="w-full text-sm" wire:model.debounce.4s="category.remarks" placeholder="type.."></textarea>
-
-                            </div>
-                            <p class="text-sm text-gray-700">
-                                Opmerkingen
-                            </p>
-                        </div>
+                        <div class="grid grid-cols-2 gap-4">
 
 
 
 
+                            <div class="px-6 py-4 my-5">
 
-
-                        <div class="px-6 pt-4 pb-2">
-
-
-                            <div class="my-5">
-
-
+                                <label class="text-sm" for="opmerkingen">Debet of Credit</label><br />
                                 <div class="form-row">
                                     <div class="pl-4 form-group col-md-1">
                                         <input type="radio" checked wire:model="category.plus_min_int" value="1">
@@ -132,14 +121,34 @@
                                     </p>
                                 </div>
 
+                            </div>
 
+
+
+
+                            <div class="">
+                                <div class="flex justify-end px-6 py-4 mb-3">
+                                    @if ($category->plus_min_int == 1)
+                                        <div class="text-white btn bg-takgreen-500 btn-big"><i class="fa fas fa-plus "
+                                                aria-hidden="true"></i>
+                                        </div>
+                                    @else
+                                        <div class="text-white btn bg-takred-500 btn-big"><i class="fa fas fa-minus "
+                                                aria-hidden="true"></i>
+                                        </div>
+                                    @endif
+                                </div>
                             </div>
 
 
 
 
 
+                        </div>
 
+
+
+                        <div class="grid grid-cols-2 gap-4 px-6 py-4">
 
                             <div>
                                 <x-button type="submit" class="justify-center w-48 mt-5">
@@ -150,7 +159,7 @@
 
 
                             @if (isset($category))
-                                <div class="mt-10">
+                                <div class="mt-5">
                                     <button
                                         class="px-4 py-1 text-sm text-black bg-gray-100 border border-gray-600 rounded hover:bg-gray-300">
                                         <a href="{{ route('category.oncategory', $category->slug) }}">
@@ -163,18 +172,19 @@
                         </div>
                     </div>
                 </div>
-
-
-            </form>
-
-
-
-
-
         </div>
 
 
+        </form>
+
+
+
+
+
     </div>
+
+
+</div>
 
 
 
