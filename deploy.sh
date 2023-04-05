9085d6f0d07c9c6fb364f20d1a2f0ba8513df33d
M Takken <martintakken@gmail.com>
2023-04-05 13:20:48 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# eerst keer: chmod 770 deploy.sh 
+
+php artisan down --refresh=15
+
+git pull
+
+# php composer.phar install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+# composer install
+
+php artisan migrate
+
+# Clear caches
+php artisan cache:clear
+
+# clear config
+php artisan config:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:cache
+
+# Clear and cache views
+php artisan view:cache
+
+# Install node modules
+npm ci
+
+# Build assets using Vite
+npm run build
+
+# Turn off maintenance mode
+php artisan up
+
