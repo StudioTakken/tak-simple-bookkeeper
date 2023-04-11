14fb23f929c4ef7bf408deb7b4b119fc671977ab
M Takken <martintakken@gmail.com>
2023-04-11 16:56:43 +0200

    not yet

--- tests/Unit/BookingTest.php
+++ tests/Unit/BookingTest.php
@@ -7,27 +7,21 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 // uses(RefreshDatabase::class);
 
-it('displays debiteuren pagina', function () {
-    $user = User::factory()->create();
-    $response = $this
-        ->actingAs($user)
-        ->get('/debiteuren');
-    $response->assertSee('debiteuren');
-});
 
 
-it('can import file', function () {
-    $user = User::find(1);
-    $this->actingAs($user);
-    $importer = new BookingController();
-    $importer->import();
-    $bookingCount = Booking::count();
 
-    $total = (int)Booking::sum('amount');
+// it('can import file', function () {
+//     $user = User::find(1);
+//     $this->actingAs($user);
+//     $importer = new BookingController();
+//     $importer->import();
+//     $bookingCount = Booking::count();
 
-    $this->assertTrue($bookingCount === 42);
-    $this->assertTrue($total === 4916302);
-});
+//     $total = (int)Booking::sum('amount');
+
+//     $this->assertTrue($bookingCount === 42);
+//     $this->assertTrue($total === 4916302);
+// })->skip('dont know how yet');
 
 
 // it('can create a booking', function () {
