6fa45591295c40945e4d9bc96d62ea18dde80eb6
M Takken <martintakken@gmail.com>
2023-04-14 13:55:03 +0200

    company settings via .env

---
+++ config/company.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'name' => env('COMPANYNAME', 'Studio Takken'),
+    'address' => env('COMPANYADDRESS', ''),
+    'zip' => env('COMPANYZIP', ''),
+    'city' => env('COMPANYCITY', ''),
+    'country' => env('COMPANYCOUNTRY', ''),
+    'email' => env('COMPANYEMAIL', ''),
+    'website' => env('COMPANYWEBSITE', ''),
+    'phone' => env('COMPANYPHONE', ''),
+    'bankaccount' => env('COMPANYBANKACCOUNT', ''),
+    'bankaccountname' => env('COMPANYBANKACCOUNTNAME', ''),
+    'kvknumber' => env('COMPANYKVKNUMBER', ''),
+    'vatnumber' => env('COMPANYVATNUMBER', ''),
+    'vatidnumber' => env('COMPANYVATIDNUMBER', ''),
+];
