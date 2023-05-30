17f845e12b7abc9f7a56be729ebc27a019619e43
M Takken <martintakken@gmail.com>
2023-05-30 12:20:29 +0200

    cleanup

--- my.sh
+++
@@ -1,8 +0,0 @@
-// php artisan make:controller BookingController --resource
-
-// php artisan make:controller BookingController --resource --model=Booking
-
-// php artisan make:model Booking -mcr
-
-// what does this mcr do?
-
